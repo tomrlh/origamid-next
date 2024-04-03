8f31d055489b3b077348644c43d56782f1df5b16
tomrlh <sithiagomedeiros@gmail.com>
2024-04-03 00:39:45 -0300

    Exercício finalizado/corrigido

--- src/app/cursos/[curso]/[aula]/page.tsx
+++ src/app/cursos/[curso]/[aula]/page.tsx
@@ -1,19 +1,29 @@
 import { Aula } from "@/types/Curso";
-import { PageParams } from "@/types/PageParams";
+import Link from "next/link";
+
+export type PageParams = {
+  params: {
+    curso: string;
+    aula: string;
+  };
+};
 
 export default async function LessonPage({ params }: PageParams) {
   console.log(params);
 
   const response = await fetch(
-    `https://api.origamid.online/cursos/${params.slug[0]}/${params.slug[1]}${params.slug[2]}`
+    `https://api.origamid.online/cursos/${params.curso}/${params.aula}`
   );
   const data = (await response.json()) as Aula;
   console.log(data);
 
   return (
     <div>
+      <Link href={`/cursos/${params.curso}`}>Voltar ao curso</Link>
       <h4>{data.nome}</h4>
       <p>{data.descricao}</p>
+      <p>Duração: {data.tempo}</p>
+      <br />
     </div>
   );
 }
--- src/app/cursos/[curso]/page.tsx
+++ src/app/cursos/[curso]/page.tsx
@@ -1,18 +1,24 @@
 import { Curso } from "@/types/Curso";
-import { PageParams } from "@/types/PageParams";
 import Link from "next/link";
+
+export type PageParams = {
+  params: {
+    curso: string;
+  };
+};
 
 export default async function CoursePage({ params }: PageParams) {
   console.log(params);
 
   const response = await fetch(
-    `https://api.origamid.online/cursos/${params.slug}`
+    `https://api.origamid.online/cursos/${params.curso}`
   );
   const curso = (await response.json()) as Curso;
   console.log(curso);
 
   return (
     <div key={curso.id}>
+      <Link href={`/cursos`}>Todos os cursos</Link>
       <h4>{curso.nome}</h4>
       <h5>
         {curso.total_aulas} aulas, {curso.total_horas} horas
@@ -21,9 +27,7 @@ export default async function CoursePage({ params }: PageParams) {
       <ul>
         {curso.aulas.map((aula) => (
           <li key={aula.id}>
-            <Link
-              href={`https://api.origamid.online/cursos/${params.slug}/${aula.slug}`}
-            >
+            <Link href={`/cursos/${params.curso}/${aula.slug}`}>
               {aula.nome}
             </Link>
           </li>
