6414bbb63cd94fefe87369abc800a00841789b02
tomrlh <sithiagomedeiros@gmail.com>
2024-04-06 00:26:15 -0300

    Salvando e usando o token com cookies (next/headers)

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,3 +1,5 @@
+import { cookies } from "next/headers";
+
 export async function GET() {
   const response = await fetch("https://api.origamid.online/conta/login", {
     method: "POST",
@@ -14,6 +16,17 @@ export async function GET() {
     return Response.json({});
   }
   const data = await response.json();
+
+  // faz o cookie seguro
+  const cookiesParams = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  cookies().set("token", data.token, {
+    httpOnly: true,
+    secure: true,
+  });
 
   return Response.json(data);
 }
