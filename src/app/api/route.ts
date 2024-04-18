bc58b8e7c77b294585eca579391f0e167531910a
tomrlh <sithiagomedeiros@gmail.com>
2024-04-17 23:21:13 -0300

    Server Actions vs Route Handlers

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
 
 export async function GET() {
   const response = await fetch("https://api.origamid.online/conta/login", {
@@ -29,4 +30,10 @@ export async function GET() {
   });
 
   return Response.json(data);
+}
+
+export async function POST(request: NextRequest) {
+  const param = request.nextUrl.searchParams.get("busca");
+  const body = await request.json();
+  return Response.json({ param, body });
 }
